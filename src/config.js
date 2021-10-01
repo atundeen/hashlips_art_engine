2986e5c7cec142643eedabe8947a59dc319488aa
lucky2025 <84954483+lucky2025@users.noreply.github.com>
2021-10-01 20:52:14 +0200

    Update config.js
    Updated for Solana

--- src/config.js
+++ src/config.js
@@ -5,20 +5,25 @@ const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
-  "This is the description of your NFT project, remember to replace this";
-const baseUri = "ipfs://NewUriToReplace";
+  "Loot the Solana space for randomly generated items and abilities.";
+const baseUri = "";
+
+//Added metadata for solana
+const collectionName = "Name Of Collection";
+const symbol = "Symbol Of Collection";
+//Define how much % you want from second market sales 1000 = 10%
+const seller_fee_basis_points = 1000;
+const external_url = "https://yourwebsite.com";
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 10,
+    growEditionSizeTo: 9999,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "layer1" },
+      { name: "layer2" },
+      { name: "layer3" },
+      { name: "layer4" },
+      { name: "layer5" },
     ],
   },
 ];
@@ -28,12 +33,12 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1000,
+  height: 1000,
 };
 
 const background = {
-  generate: true,
+  generate: false,
   brightness: "80%",
 };
 
@@ -50,10 +55,36 @@ const preview = {
   imageName: "preview.png",
 };
 
+//Added metadata for solana
+//Leave uri and type at the current way because they are placeholders for arweave
+const properties = {
+  files: [
+      {
+        "uri": "image.png",
+        "type": "image/png"
+      },
+    ],
+  category: "image",
+  creators: [
+    {
+      "address": "Your Solana Address",
+      "share": 100
+    }
+  ]
+};
+
 module.exports = {
   format,
   baseUri,
   description,
+
+  //Added metadata for solana
+  collectionName,
+  symbol,
+  seller_fee_basis_points,
+  external_url,
+  properties, 
+
   background,
   uniqueDnaTorrance,
   layerConfigurations,
